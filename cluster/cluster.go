880fc8956406d667a02251a3b4a53b1eca8f0b98
Solomon Jacobs <solomonjacobs@protonmail.com>
2025-12-13 23:20:48 +0100

    remove some minor lints

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -11,6 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cluster uses `hashicorp/memberlist` to maintain a list of
+// alertmanagers, which then exchange
+// * a notification log,
+// * periodically check the health of the other members,
+// * can forward silences.
 package cluster
 
 import (
